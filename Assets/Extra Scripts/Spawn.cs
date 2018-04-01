7df3af2e26547486ae94e4f604562aaf06d23c00
StoicanDragos <bv28.dago@gmail.com>
2018-04-01 15:32:23 +0300

    saved

---
+++ Assets/Extra Scripts/Spawn.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawn : MonoBehaviour {
+
+	private float timer = 5f;
+	public GameObject enemy;
+	private Vector3 spawnPoz;
+	private float axisY = 0.0f;
+	private float axisX = 0.0f;
+
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		///enemyTransform = GetComponent <Transform> ();
+		InvokeRepeating ("Spawner", timer, timer);
+
+	}
+
+
+	// Update is called once per frame
+	
+
+	void Update () 
+	{
+		
+	}
+
+	void Spawner () 
+	{
+		if (enemy.name == "Enemy") 
+		{
+			axisY = Random.Range (-30f, 30f);
+			axisX = Random.Range (-62f, 62f);
+			spawnPoz = new Vector3 (axisX, axisY, -1.5f);
+			Object.Instantiate (enemy, spawnPoz, Quaternion.identity);
+		}
+	}
+}
