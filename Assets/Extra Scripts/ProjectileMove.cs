75a380f25d6c6d652462726f6f6223a97b2988b9
StoicanDragos <bv28.dago@gmail.com>
2018-04-01 15:11:52 +0300

    Initial commit

---
+++ Assets/Extra Scripts/ProjectileMove.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileMove : MonoBehaviour {
+
+	private Vector3 MousePoz;
+	private Vector3 movement;
+	private Transform projectileTransform;
+	public Transform playerTransform;
+	private Vector3 target;
+	private float speed= 20f;
+	private GameObject objectt;
+
+	// Use this for initialization
+	void Start () 
+	{
+		projectileTransform = GetComponent <Transform> ();
+		projectileTransform.rotation = playerTransform.rotation;
+		objectt = GetComponent <GameObject> ();
+		target = Camera.main.ScreenToWorldPoint (Input.mousePosition) - playerTransform.position;
+		Physics.IgnoreCollision (playerTransform.GetComponent<Collider> (), GetComponent<Collider> ());
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+
+	void FixedUpdate ()
+	{
+		float step = speed * Time.deltaTime;
+
+		projectileTransform.position += projectileTransform.right;
+
+
+
+	}
+	void OnCollisionEnter (Collision col)
+	{
+		if(col.gameObject.tag == "Enemy" || col.gameObject.tag == "Wall")
+		{
+			Destroy(gameObject);
+		}
+	}
+}
